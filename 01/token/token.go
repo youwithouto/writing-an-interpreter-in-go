0624d4c307d2e874348fe00f5b48225eeae650a9
Yu Zhao <yuzhao@192-168-1-4.tpgi.com.au>
2018-01-05 01:20:56 +1100

    Add Type definitions

--- 01/token/token.go
+++ 01/token/token.go
@@ -6,3 +6,29 @@ type Token struct {
 	Type    TokenType // The type of the current token
 	Literal string    // The literal value of the current token
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"   // number
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
